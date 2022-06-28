d6cd86e2d6e781023185de5c2af65b3169943f38
Daniel Reyes <86884359+DanielDevHN@users.noreply.github.com>
2022-06-28 13:38:17 -0600

    Agregando petición para obtener clientes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import { useEffect } from 'react'
 
 const Home = () => {
+  
+    useEffect(() => {
+        const obtenerClientesAPI = async () => {
+            try {
+                
+            } catch (error) {
+                console.log(error)
+            }
+        }
+
+        obtenerClientesAPI()
+    }, [])
+
   return (
     <div>
         <h1>Home.jsx</h1>
