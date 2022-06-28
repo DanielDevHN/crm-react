be04dbaa1a0382d5409c0b2f7442b065d83f130a
Daniel Reyes <86884359+DanielDevHN@users.noreply.github.com>
2022-06-28 14:05:55 -0600

    Agregando tabla de clientes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,19 @@
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
+import Cliente from '../components/Cliente'
+
 
 const Home = () => {
+
+    const [clientes, setClientes] = useState([])
   
     useEffect(() => {
         const obtenerClientesAPI = async () => {
             try {
-                
+                const url = 'http://localhost:4000/clientes'
+                const respuesta = await fetch(url)
+                const resultado = await respuesta.json()
+
+                setClientes(resultado)
             } catch (error) {
                 console.log(error)
             }
@@ -16,7 +24,30 @@ const Home = () => {
 
   return (
     <div>
-        <h1>Home.jsx</h1>
+        <>
+            <h1 className="font-black text-3xl text-blue-900">Clientes</h1>
+            <p className="mt-3">Administra tus clientes</p>
+
+            <table className="w-full mt-5 table-auto shadow bg-white">
+                <thead className="bg-blue-800 text-white">
+                    <tr>
+                        <th className="p-2">Nombre</th>
+                        <th className="p-2">Contacto</th>
+                        <th className="p-2">Empresa</th>
+                        <th className="p-2">Acciones</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {clientes.map(cliente => (
+                        <Cliente 
+                            key={cliente.id}
+                            cliente={cliente}
+                        /> 
+                    ))}
+                </tbody>
+            </table>
+        </>
     </div>
   )
 }
