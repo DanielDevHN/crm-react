10f5c5290c8c587a75a7126fc31ff0775b7fbcd7
Daniel Reyes <86884359+DanielDevHN@users.noreply.github.com>
2022-06-28 21:32:09 -0600

    Agregando listado de Clientes por ID

---
+++ src/pages/ViewCustomer.jsx
@@ -0,0 +1,71 @@
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+
+const ViewCustomer = () => {
+
+    const [cliente, setCliente] = useState({})
+    const [cargando, setCargando] = useState(false)
+
+    const { id } = useParams()
+
+    useEffect(() => {
+        setCargando(!cargando)
+        const obtenerClientesAPI = async () => {
+            try {
+                const url = `http://localhost:4000/clientes/${id}`
+                const respuesta = await fetch(url)
+                const resultado = await respuesta.json()
+
+                setCliente(resultado)
+            } catch (error) {
+                console.log(error)
+            }
+            setCargando(false)
+        }
+        obtenerClientesAPI()
+    }, [])
+
+    return (
+
+        Object.keys(cliente).length === 0 ? <p>No hay Resultados</p> : (
+
+            <div className="text-3xl text-gray-700">
+                {cargando ? 'Cargando...' : (
+                    <>
+                        <h1 className="font-black text-3xl text-blue-900">Detalles del Cliente : {cliente.nombre}</h1>
+                        <p className="mt-3 text-xl font-semibold">Información del Cliente</p>
+
+                        <p className="text-2xl text-gray-600  mt-10">
+                            <span className="text-gray-800  uppercase font-bold">Cliente: </span>
+                            {cliente.nombre}
+                        </p>
+                        <p className="text-2xl text-gray-600 mt-4">
+                            <span className=" text-gray-800 uppercase font-bold">Email: </span>
+                            {cliente.email}
+                        </p>
+                        {cliente.telefono && (
+                            <p className="text-2xl text-gray-600 mt-4">
+                                <span className="text-gray-800  uppercase font-bold">Teléfono: </span>
+                                {cliente.telefono}
+                            </p>
+                        )}
+
+                        <p className="text-2xl text-gray-600 mt-4">
+                            <span className=" text-gray-800 uppercase font-bold">Empresa: </span>
+                            {cliente.empresa}
+                        </p>
+
+                        {cliente.notas && (
+                            <p className="text-2xl text-gray-600 mt-4">
+                                <span className=" text-gray-800 uppercase font-bold">Notas: </span>
+                                {cliente.notas}
+                            </p>
+                        )}
+                    </>
+                )}
+            </div>
+        )
+    )
+}
+
+export default ViewCustomer
