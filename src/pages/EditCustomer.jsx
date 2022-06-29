b6d67c7d661aba695ce52ff23e1b0027040c00bf
Daniel Reyes <86884359+DanielDevHN@users.noreply.github.com>
2022-06-29 00:43:19 -0600

    Trabajando vista editar clientes

--- src/pages/EditCustomer.jsx
+++ src/pages/EditCustomer.jsx
@@ -1,12 +1,39 @@
-import { Outlet } from "react-router-dom"
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import Form from "../components/Formulario"
 
 const EditCustomer = () => {
-  return (
-    <div>
-        <h1>Editar Cliente</h1>
 
-        <Outlet />
-    </div>
+    const [cliente, setCliente] = useState({})
+    const [cargando, setCargando] = useState(true)
+    const { id } = useParams()
+
+    useEffect(() => {
+        const obtenerClientesAPI = async () => {
+            try {
+                const url = `http://localhost:4000/clientes/${id}`
+                const respuesta = await fetch(url)
+                const resultado = await respuesta.json()
+
+                setCliente(resultado)
+            } catch (error) {
+                console.log(error)
+            }
+   
+            setCargando(!cargando)
+        }
+        obtenerClientesAPI()
+    }, [])
+
+  return (
+    <>
+        <h1 className="font-black text-3xl text-blue-900">Editar Cliente</h1>
+        <p className="mt-3">Utiliza este formulario para editar los datos de un cliente.</p>
+
+        <Form
+            cliente={cliente}
+        />
+    </>
   )
 }
 
